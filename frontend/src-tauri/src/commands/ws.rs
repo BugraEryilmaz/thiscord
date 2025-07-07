0402c823822ee8402f390a9fd5e50f70217f9374
Bugra Eryilmaz <bugra.eryilmaz@epfl.ch>
2025-07-07 22:58:43 +0200

    tokio spawn local bug fix

--- frontend/src-tauri/src/commands/ws.rs
+++ frontend/src-tauri/src/commands/ws.rs
@@ -1,30 +1,48 @@
 use front_shared::LoginStatus;
 use tauri::{http, AppHandle};
-use tokio::sync::mpsc::Receiver;
+use tokio::{
+    runtime::Builder, sync::mpsc::Receiver, task::LocalSet
+};
 
-use crate::{commands::{check_cookies, logout}, websocket::{websocket_handler, WebSocketRequest}, Error};
+use crate::{
+    commands::{check_cookies, logout},
+    websocket::{websocket_handler, WebSocketRequest},
+    Error,
+};
 
 pub fn connect_ws(handle: AppHandle, mut rx: Receiver<WebSocketRequest>) {
-    tokio::task::spawn_local(async move {
-        loop {
-            // Check for cookies
-            let handle = handle.clone();
-            if !matches!(check_cookies(handle.clone()).await, Ok(LoginStatus::LoggedIn(_))) {
-                tracing::warn!("No cookies found, cannot connect to WebSocket.");
-                return;
-            }
+    let rt = Builder::new_current_thread().enable_all().build().unwrap();
 
-            let result = websocket_handler(handle.clone(), &mut rx).await;
-            match &result {
-                Err(Error::TokioTungsteniteError(tokio_tungstenite::tungstenite::Error::Http(e))) => {
-                    if e.status() == http::StatusCode::UNAUTHORIZED {
-                        let _ = logout(handle.clone()).await;
-                        tracing::warn!("Unauthorized access detected, logging out.");
-                    }
+    std::thread::spawn(move || {
+        let local = LocalSet::new();
+
+        local.spawn_local(async move {
+            loop {
+                // Check for cookies
+                let handle = handle.clone();
+                if !matches!(
+                    check_cookies(handle.clone()).await,
+                    Ok(LoginStatus::LoggedIn(_))
+                ) {
+                    tracing::warn!("No cookies found, cannot connect to WebSocket.");
+                    return;
                 }
-                _ => {}
+
+                let result = websocket_handler(handle.clone(), &mut rx).await;
+                match &result {
+                    Err(Error::TokioTungsteniteError(
+                        tokio_tungstenite::tungstenite::Error::Http(e),
+                    )) => {
+                        if e.status() == http::StatusCode::UNAUTHORIZED {
+                            let _ = logout(handle.clone()).await;
+                            tracing::warn!("Unauthorized access detected, logging out.");
+                        }
+                    }
+                    _ => {}
+                }
+                tracing::error!("WebSocket handler error: {:?}", result);
             }
-            tracing::error!("WebSocket handler error: {:?}", result);
-        }
+        });
+        rt.block_on(local);
     });
 }
